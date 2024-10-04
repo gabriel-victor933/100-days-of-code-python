91c76aeaa4e73758465504bdc75a8e312f11a465
gabriel-victor933 <gabrielvictor933@usp.br>
2024-10-04 08:46:44 -0300

    feat: finishing day 1

--- day-01/main.py
+++ day-01/main.py
@@ -1,6 +1,11 @@
 
 def main(): 
-    print('hello word')
+    print("Welcome to the Band Name Generator.",3)
 
+    s = input('What is the name of the city you grew up in? ')
+
+    s1 = input('What\'s your pet\'s name? ')
+
+    print('Your band name could be '+ s + " " +  s1)
 
 main()
