59f6aa583e2b1c98bdf2fd9b654f76db3d56257a
gabriel-victor933 <gabrielvictor933@usp.br>
2024-10-24 15:54:38 -0300

    day 25

--- day-25/main.py
+++ day-25/main.py
@@ -1,6 +1,46 @@
+from turtle import Screen, Turtle
 import pandas
+import time
 
-data = pandas.read_csv('day-25/weather_data.csv')
+data = pandas.read_csv("day-25/50_states.csv")
 
-print(data["temp"].median())
-print(data["temp"].max())
+states = data['state'].to_list()
+lower_state = list(map(lambda text: text.lower(),states))
+
+x_cord = data['x'].to_list()
+y_cord = data['y'].to_list()
+
+turtle = Turtle()
+drawer = Turtle(visible=False)
+screen = Screen()
+screen.title('U.S States game')
+img = 'day-25/blank_states_img.gif'
+screen.addshape(img)
+
+turtle.shape(img)
+drawer.penup()
+
+num_state = 0
+
+while num_state < 50:
+    state = screen.textinput(f"{num_state}/50 State Correct","Insert the name of the state: ").lower()
+
+    if state == 'exit':
+        break
+
+    if state in lower_state:
+
+        index = lower_state.index(state)
+
+        drawer.goto(x_cord[index],y_cord[index])
+
+        drawer.write(states[index],False,"center",("arial",8,"normal"))
+
+        num_state += 1
+
+        lower_state.remove(state)
+
+        time.sleep(0.5)
+    
+
+screen.bye()
